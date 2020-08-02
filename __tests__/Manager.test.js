486602ac93b14cb588dd3d7720856c07b789e30f
Alexsis Conley Brown <alexsismcb@gmail.com>
2020-08-01 19:34:19 -0500

    manager info

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,14 @@
+const Manager = require('../lib/Manager.js');
+
+test('creates Employee object with properties', () => {
+    const manager = new Manager('Alexsis', '1', 'name@email.com', '123-4567');
+
+    expect(manager.name).toBe('Alexsis');
+    expect(manager.id).toBe('1');
+    expect(manager.email).toBe('name@email.com');
+    expect(manager.officeNumber).toBe('123-4567');
+
+    console.log(manager);
+});
+
+
