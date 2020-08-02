43c7069f320843ceeb43c0aad449fd47f82e1482
Alexsis Conley Brown <alexsismcb@gmail.com>
2020-08-02 06:54:46 -0500

    added manager properties

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,14 +1,34 @@
 const Manager = require('../lib/Manager.js');
 
 test('creates Employee object with properties', () => {
-    const manager = new Manager('Alexsis', '1', 'name@email.com', '123-4567');
+    const manager = new Manager('Alexsis', 1, 'name@email.com', '123-4567');
 
-    expect(manager.name).toBe('Alexsis');
-    expect(manager.id).toBe('1');
-    expect(manager.email).toBe('name@email.com');
-    expect(manager.officeNumber).toBe('123-4567');
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(String));
 
     console.log(manager);
 });
 
+test("get the team member's name", () => {
+    const manager = new Manager('Alexsis', 1, 'name@email.com', '123-4567');
+
+    expect(manager.getName()).toEqual(expect.stringContaining(manager.name.toString()));
+    console.log(manager.getName());
+});
+
+test("get team member's ID number", () => {
+    const manager = new Manager('Alexsis', 1, 'name@email.com', '123-4567');
+
+    expect(manager.getId()).toEqual(expect.stringContaining(manager.id.toString()));
+    console.log(manager.getId());
+});
+
+test("get team member's email address", () => {
+    const manager = new Manager('Alexsis', 1, 'name@email.com', '123-4567');
+
+    expect(manager.getEmail()).toEqual(expect.stringContaining(manager.email.toString()));
+    console.log(manager.getEmail());
+});
 
