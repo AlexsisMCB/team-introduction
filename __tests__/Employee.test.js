91858f92967e80a30775c8ce6a052f43d0273afe
Alexsis Conley Brown <alexsismcb@gmail.com>
2020-08-02 07:51:09 -0500

    employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,32 @@
 const Employee = require('../lib/Employee.js');
 
+test('creates employee object with properties', () => {
+    const employee = new Employee('Employee', 0, 'name@email.com');
+
+    expect(employee.name).toEqual(expect.any(String));;
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+
+    console.log(employee);
+});
+
+test("get the team member's name", () => {
+    const employee = new Employee('Employee', 0, 'name@email.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+    console.log(employee.getName());
+});
+
+test("get team member's ID number", () => {
+    const employee = new Employee('Employee', 0, 'name@email.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+    console.log(employee.getId());
+});
+
+test("get team member's email address", () => {
+    const employee = new Employee('Employee', 0, 'name@email.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+    console.log(employee.getEmail());
+});
